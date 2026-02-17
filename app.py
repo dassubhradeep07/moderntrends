fc5dc20b23975e01e99dc7c5eee3f98d8e423227
Subhradeep_07 <dassubhradeep07@gmail.com>
2026-02-17 07:27:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -144,9 +144,10 @@ for i,t in enumerate(types):
         if st.button("Explore", key=t):
             st.session_state["category"]=t
             #st.switch_page("/workspaces/moderntrends/pages/Products.py")
-            st.switch_page("/pages/Products.py")
+            st.switch_page("pages/Products.py")
 
         st.markdown('</div>', unsafe_allow_html=True)
+
 
 
 
