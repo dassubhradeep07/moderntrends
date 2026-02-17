e7b4546b3b79baa36789833f19f0731939b976b2
Subhradeep_07 <dassubhradeep07@gmail.com>
2026-02-17 07:33:02 +0530

    Refactor Products.py for improved readability

--- pages/Products.py
+++ pages/Products.py
@@ -124,9 +124,10 @@ for i,row in data.iterrows():
         if st.button("View",key=row["code"]):
             st.session_state["product"]=row["code"]
             #st.switch_page("Product_Details.py")
-            st.switch_page("pages/Product_Details")
+            st.switch_page("pages/Product_Details.py")
 
         st.markdown('</div>', unsafe_allow_html=True)
+
 
 
 
